413f022755a8622d61765dbb914dcfbe0cb797db
Vegard Vesterheim <vegard.vesterheim@uninett.no>
2018-11-19 09:55:46 +0100

    Moved to gitlab

--- data/api_key_sample.js
+++
@@ -1,7 +0,0 @@
-// Fix parameters here, and save file as 'api_key.js'
-api_data = {
-    "local_api_key"   : "API-key for brewersfriend",
-    "local_api_email" : "API-email for brewersfriend", 
-    "local_api_url"   : "API-url from brewersfriend"
-};
-
